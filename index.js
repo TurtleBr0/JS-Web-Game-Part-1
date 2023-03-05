e27590dafce2d602e82c0a5fcd528ab3095e8cec
TurtleBr0 <jacobheier@gmail.com>
2023-03-04 20:31:28 -0800

    homework

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
+}
+function newItem(url, left, bottom,) {
+    let object = newImage(url, left, bottom)
+    
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 450)
+newImage('assets/pillar.png', 350, 250)
+newImage('assets/pine-tree.png', 450, 350)
+newImage('assets/crate.png', 150, 350)
+newImage('assets/well.png', 500, 575)
+newItem('assets/sword.png', 500, 555)
+newItem('assets/sheild.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
+
